436acf09864db4cac6931ebe058dc70e393233e1
deependerrathore <deepak.republic@gmail.com>
2014-10-06 00:25:10 +0530

    Implementing the Insert method for Inserting at the beginning. 1st -> for Inserting at the first time and 2nd->when there is already one or more element in the list

--- DSA/main.c
+++ DSA/main.c
@@ -1,9 +1,36 @@
 //Linked List :  Inserting a node at beginning
 #include <stdio.h>
 #include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* next;
+};
 
+struct Node* head;  //global variable , can be accessed anywhere
+
+void Insert(int x){
+    Node* temp = malloc(sizeof(struct Node)) ;
+    (*temp).data =x ;   //or temp ->data = x ;
+    (*temp).next = NULL;//or temp->next = NULL;
+    if(head !=NULL) (*temp).next = head;
+    head = temp;
+
+}
+void Insert(int x);
+void Print();
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    head = NULL ; //empty list
+    printf("How many number?\n ");
+
+    int n, i ,x ;
+    scanf("%d",&n);
+
+    for(i = 0 ; i<n ; i++){
+        printf("Enter the number\n ");
+        scanf("%d",x);
+        Insert(x);
+        Print();
+    }
+
 }

7adb5eb01833269cf8427bdcd5022a5fa4024895
deependerrathore <deepak.republic@gmail.com>
2014-10-06 01:39:08 +0530

    Linked List : When we declare head variable locally in main , Not as globally

--- DSA/main.c
+++ DSA/main.c
@@ -6,29 +6,28 @@ struct Node{
     struct Node* next;
 };
 
-struct Node* head;  //global variable , can be accessed anywhere
-
-void Insert(int x){
+struct Node* Insert(struct Node* head , int x){
     struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
     (*temp).data =x;
     (*temp).next = head;
     head = temp;
+    return head;
 
 }
 
-void Print(){
-    struct Node* temp = head;
+void Print(struct Node* head){
+
     printf("List is : ");
-    while(temp != NULL){
-        printf("%d ",(*temp).data);
-        temp = (*temp).next;
+    while(head != NULL){
+        printf("%d ",(*head).data);
+        head = (*head).next;
     }
     printf("\n ");
 
 }
 int main()
 {
-    head = NULL ; //empty list
+    struct Node* head = NULL ; //empty list
     printf("How many number?\n ");
 
     int n, i ,x ;
@@ -37,8 +36,8 @@ int main()
     for(i = 0 ; i<n ; i++){
         printf("Enter the number\n ");
         scanf("%d",&x);
-        Insert(x);
-        Print();
+        head = Insert(head,x);
+        Print(head);
     }
 
 }

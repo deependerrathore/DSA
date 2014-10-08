3f2b2cbd58f77d51938c45f752c90248a334abfe
deependerrathore <deepak.republic@gmail.com>
2014-10-08 19:56:48 +0530

    Variables in C

---
+++ DSA/C_program/simpleInterest.c
@@ -0,0 +1,16 @@
+//using Constant and Variable
+#include<stdio.h>
+int main(){
+    int a;      //variable declaration
+    a = 12;     //initialization
+    int b = 13;
+    char c;
+    float real_number;
+    real_number = 3.14;
+
+    c = 'c';
+    printf("a = %d",a); //Printing the value in the integer variable
+    printf("\ncharacter is %c",c);
+    printf("\nthis is the float number %f",real_number);
+    return 0;
+}

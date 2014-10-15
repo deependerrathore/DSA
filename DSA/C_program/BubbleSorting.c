0d777f3915c46c7653f77e30c31a6bfa56db102a
deependerrathore <deepak.republic@gmail.com>
2014-10-15 17:58:47 +0530

    BubbleSort Program but efficient

--- DSA/C_program/BubbleSorting.c
+++ DSA/C_program/BubbleSorting.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 void main(){
-    int a[] = {2,10,600,200,800,0,7,5,6};
+    int a[] = {5,1,6,2,4,3,7};
     int i , j , temp;
 
     for(i=0;i<7;i++){
+        int flag = 0 ;      //taking a flag variable
         for(j=0;j<7-i-1;j++){
+
+
+
             printf("The value of i and j is %d %d\n",i,j);
+
             if(a[j]> a[j+1]){
                 temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] =temp;
+                flag = 1;       //setting flag as 1 , if swapping occurs
             }
+        }
+        if(!flag){              //breaking out of for loop if no swapping takes place
+            break;
         }
     }
 
